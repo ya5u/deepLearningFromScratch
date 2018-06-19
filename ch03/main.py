a1111bc9798ad3314d29117a81bea3e385b89909
Yasutaka Shinzaki <y.lax.shinzaki43@gmail.com>
2018-06-19 19:19:30 +0900

    done 3.2.7

--- ch03/main.py
+++ ch03/main.py
@@ -7,10 +7,12 @@ def step_function(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def relu(x):
+    return np.maximum(0, x)
 
 x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
+y = relu(x)
 
 plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
+plt.ylim(-0.1, 5.1)
 plt.show()

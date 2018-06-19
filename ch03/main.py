8a01ef1d77fee140f67fd0edf981b121fcbe2de0
Yasutaka Shinzaki <y.lax.shinzaki43@gmail.com>
2018-06-19 19:50:04 +0900

    done 3.4.2

--- ch03/main.py
+++ ch03/main.py
@@ -10,9 +10,45 @@ def sigmoid(x):
 def relu(x):
     return np.maximum(0, x)
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = relu(x)
+def identity_function(x):
+    return x
 
-plt.plot(x, y)
-plt.ylim(-0.1, 5.1)
-plt.show()
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+print(W1.shape)
+print(X.shape)
+print(B1.shape)
+
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid(A1)
+
+print(A1)
+print(Z1)
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+print(Z1.shape)
+print(W2.shape)
+print(B2.shape)
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+print(A2)
+print(Z2)
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+print(Z2.shape)
+print(W3.shape)
+print(B3.shape)
+
+A3 = np.dot(Z2, W3) + B3
+Y = identity_function(A3)
+
+print(A3)
+print(Y)
